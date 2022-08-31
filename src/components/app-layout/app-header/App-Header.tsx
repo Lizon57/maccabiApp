995ba5d206e5bf53849be0dba2cab4de3088639f
O Y <orenyaniv90@gmail.com>
2022-08-31 23:28:16 +0300

    Add brand logo

--- src/components/app-layout/app-header/App-Header.tsx
+++ src/components/app-layout/app-header/App-Header.tsx
@@ -1,11 +1,17 @@
 import BEMHelper from "react-bem-helper"
 
+import logo from '../../../assets/images/logo.png'
+
 
 const BEM_HELPER = new BEMHelper('app-header')
 
 
 export const AppHeader = () => {
     return (
-        <div {...BEM_HELPER('container')}>תפריט</div>
+        <div {...BEM_HELPER('container')}>
+            <div {...BEM_HELPER('content')}>
+                <img src={logo} alt="עמוד ראשי" title="עמוד ראשי" {...BEM_HELPER('brand-logo')} />
+            </div>
+        </div>
     )
 }
