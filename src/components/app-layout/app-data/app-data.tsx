dfeb6eaac4db29de24d7d9568ffaece5d8441515
O Y <orenyaniv90@gmail.com>
2022-09-02 14:08:06 +0300

    Remove back to top cmp

--- src/components/app-layout/app-data/app-data.tsx
+++ src/components/app-layout/app-data/app-data.tsx
@@ -1,9 +1,6 @@
-import { BackToTop } from "./back-to-top/back-to-top"
-
 export const AppData = ({ footerHeight }: propsType) => {
     return (
         <aside className="app-layout--app-data__aside-container">
-            <BackToTop footerHeight={footerHeight} />
         </aside>
     )
 }
