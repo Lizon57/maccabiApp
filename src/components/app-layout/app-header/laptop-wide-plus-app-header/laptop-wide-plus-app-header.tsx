215f8c37b6a31d153a0659cb6f830a7f54ee8da1
O Y <orenyaniv90@gmail.com>
2022-09-05 18:12:33 +0300

    Improve up-to-laptop-wide-app-header  cmp stracture

--- src/components/app-layout/app-header/laptop-wide-plus-app-header/laptop-wide-plus-app-header.tsx
+++ src/components/app-layout/app-header/laptop-wide-plus-app-header/laptop-wide-plus-app-header.tsx
@@ -1,4 +1,4 @@
-import logo from "../../../../assets/images/logo.png"
+import squareLogo from "../../../../assets/images/square-logo.png"
 import { BranchChipList } from "../branch-chip/branch-chip-list"
 import { CategoryLinkList } from "../category-link/category-link-list"
 import { OptionBar } from "../option-bar/option-bar"
@@ -11,7 +11,7 @@ export const LaptopWidePlusAppHeader = () => {
                 <BranchChipList />
                 <OptionBar />
                 <div className="brand-logo-container">
-                    <img src={logo} alt="עמוד ראשי" title="עמוד ראשי" className="brand-logo" />
+                    <img src={squareLogo} alt="עמוד ראשי" title="עמוד ראשי" className="brand-logo" />
                 </div>
                 <CategoryLinkList relevant={[0, 3]} />
                 <CategoryLinkList relevant={[3]} isLeftSided={true} />
--- src/components/app-layout/app-header/up-to-laptop-wide-app-header/up-to-laptp-wide-app-header.tsx
+++ src/components/app-layout/app-header/up-to-laptop-wide-app-header/up-to-laptp-wide-app-header.tsx
@@ -1,8 +1,18 @@
+import { FaBars } from "react-icons/fa"
+import wideLogo from "../../../../assets/images/wide-logo.png"
+import { Search } from "../option-bar/search/search"
+
 export const UpToLaptopWideAppHeader = () => {
     return (
         <header className="app-layout--app-header__up-to-laptop-wide">
             <div className="content">
-                תוכן
+                <div className="options-container">
+                    <div className="options-icon-wrapper">
+                        <FaBars />
+                    </div>
+                    <img src={wideLogo} alt="עמוד ראשי" title="עמוד ראשי" className="brand-logo" />
+                </div>
+                <Search />
             </div>
         </header>
     )
