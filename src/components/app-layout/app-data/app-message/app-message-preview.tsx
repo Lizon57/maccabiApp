2f2afe2ec91d5565e371d283fae690e2c8aae600
O Y <orenyaniv90@gmail.com>
2022-09-05 09:38:00 +0300

    init circular-progress-bar cmp

--- src/components/app-layout/app-data/app-message/app-message-preview.tsx
+++ src/components/app-layout/app-data/app-message/app-message-preview.tsx
@@ -1,10 +1,16 @@
+import { GoNote } from "react-icons/go"
 import { appMessageProps } from "../../../../types/app-message"
+import { CircularProgressBar } from "../../../common/circular-progress-bar/circular-progress-bar"
+
 
 export const AppMessagePreview = ({ message }: appMessageProps) => {
     return (
         <div className="app-data--message__preview-container">
-            <div className={"preview-title " + message.type}>{message.title}</div>
+            <div className="preview-title"><GoNote /> {message.title}</div>
             <div className="preview-text">{message.text}</div>
+            <div className="preview-timer">
+                <CircularProgressBar type={message.type} />
+            </div>
         </div>
     )
 }
---
+++ src/components/common/circular-progress-bar/circular-progress-bar.tsx
@@ -0,0 +1,37 @@
+let additionalStyle: style;
+
+export const CircularProgressBar = ({ type }: propsType) => {
+    switch (type) {
+        case 'success':
+            additionalStyle = { stroke: 'green' }
+            break
+
+        case 'danger':
+            additionalStyle = { stroke: 'red' }
+            break
+            
+            case 'activity':
+            additionalStyle = { stroke: '#195da6' }
+            break
+
+        default: break
+    }
+
+    return (
+        <div className="common-cmp--circular-progress-bar">
+            <svg>
+                <circle cx="12" cy="12" r="10" style={additionalStyle ? additionalStyle : {}} />
+            </svg>
+        </div>
+    )
+}
+
+
+type propsType = {
+    type: string
+}
+
+
+type style = {
+    stroke: string
+}
