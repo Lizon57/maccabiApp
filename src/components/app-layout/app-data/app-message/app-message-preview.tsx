1609ee3b90ef7c57a6be1d60334415d5856fe616
O Y <orenyaniv90@gmail.com>
2022-09-06 00:34:42 +0300

    Improve app-message-section style

--- src/components/app-layout/app-data/app-message/app-message-preview.tsx
+++ src/components/app-layout/app-data/app-message/app-message-preview.tsx
@@ -1,15 +1,30 @@
-import { GoNote } from "react-icons/go"
+import { BsHandThumbsUp } from "react-icons/bs"
+import { BsHandThumbsDown } from "react-icons/bs"
+import { BiMessageAltDetail } from "react-icons/bi"
+
 import { appMessageProps } from "../../../../types/app-message"
-import { CircularProgressBar } from "../../../common/circular-progress-bar/circular-progress-bar"
+
+
+const getIcon = (type: string) => {
+    switch (type) {
+        case 'success':
+            return <BsHandThumbsUp />
+
+        case 'fail':
+            return <BsHandThumbsDown />
+
+        case 'message':
+            return <BiMessageAltDetail />
+
+        default: return
+    }
+}
 
 
 export const AppMessagePreview = ({ message }: appMessageProps) => {
     return (
         <div className="app-data--message__preview-container">
-            <div className="preview-timer">
-                <CircularProgressBar type={message.type} />
-            </div>
-            <div className="preview-title"><GoNote /> {message.title}</div>
+            <div className="preview-title">{getIcon(message.type)} {message.title}</div>
             <div className="preview-text">{message.text}</div>
         </div>
     )
