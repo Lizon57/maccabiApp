2d2fd35eff4fa4bcb8cdc268fa4137083c782045
O Y <orenyaniv90@gmail.com>
2022-09-14 09:54:53 +0300

    Improve game poster service query function - add debug options

--- src/services/entities/game-poster-service.ts
+++ src/services/entities/game-poster-service.ts
@@ -1,14 +1,25 @@
 import { GAME_POSTERS_DB } from "../../data/entities/game-poster/game-posters-db"
+import { gamePosterType } from "../../types/game-poster"
 import { asyncLocalStorageService } from "../async-local-storage-service"
 
 const STORAGE_KEY = 'GAME_POSTER_DB'
 
 const query = async () => {
+    let data: gamePosterType[]
     try {
-        return await asyncLocalStorageService.query(STORAGE_KEY, GAME_POSTERS_DB)
+        data = await asyncLocalStorageService.query(STORAGE_KEY, GAME_POSTERS_DB) as gamePosterType[]
     } catch (err) {
         throw new Error('שגיאה בטעינת נתונים')
     }
+
+    return data
+
+    // DEBUG:
+    // Resolve after delay
+    // return new Promise(res => setTimeout(() => res(data), 2000))
+
+    // Reject:
+    // throw new Error('שגיאה מכוונת')
 }
 
 
