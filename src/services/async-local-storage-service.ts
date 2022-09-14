1d17dc318c33aa64d537747a7ec9c0b7ad9ff4ef
O Y <orenyaniv90@gmail.com>
2022-09-14 09:45:38 +0300

    Add async local storage service

---
+++ src/services/async-local-storage-service.ts
@@ -0,0 +1,23 @@
+const query = async<T>(entityName: string, fallBackDB: T[]) => {
+    let entities: null | string | T[] = localStorage.getItem(entityName)
+
+    if (!Array.isArray(entities)) {
+        _createDB(entityName, fallBackDB)
+        entities = JSON.stringify(fallBackDB)
+    }
+
+    entities = JSON.parse(entities) as T[]
+
+    return entities
+}
+
+
+
+const _createDB = <T>(entityName: string, DB: T[]) => {
+    localStorage.setItem(entityName, JSON.stringify(DB))
+}
+
+
+export const asyncLocalStorageService = {
+    query,
+}
