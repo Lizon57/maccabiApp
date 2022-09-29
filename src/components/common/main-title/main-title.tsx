61a101053694e19f6d223f1d7b034a27486e2823
O Y <orenyaniv90@gmail.com>
2022-09-29 23:35:37 +0300

    Allow main-title cmp to get an additional cmp as prop

--- src/components/common/main-title/main-title.tsx
+++ src/components/common/main-title/main-title.tsx
@@ -1,5 +1,16 @@
-export const MainTitle = ({ text = 'כותרת ראשית', isSticky = false }) => {
+export const MainTitle = ({ text, isSticky = false, additionalCmp }: propsType) => {
     return (
-        <h2 className={'common-cmp--main-title' + (isSticky ? ' sticky' : '')}>{text}</h2>
+        <div className={'common-cmp--main-title' + (isSticky ? ' sticky' : '') + (additionalCmp ? '' : ' background-colorize')}>
+            <h2>{text}</h2>
+
+            {additionalCmp && <div className="additional-cmp-container">{additionalCmp}</div>}
+        </div>
     )
+}
+
+
+type propsType = {
+    text: string,
+    isSticky?: boolean,
+    additionalCmp?: JSX.Element
 }
