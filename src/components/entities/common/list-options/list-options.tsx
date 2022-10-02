24cee8bd2aa1df77f211f1ef026d223efb67b8b5
O Y <orenyaniv90@gmail.com>
2022-10-02 12:40:24 +0300

    Adjust more generic entities sort drop down

--- src/components/entities/common/list-options/list-options.tsx
+++ src/components/entities/common/list-options/list-options.tsx
@@ -5,28 +5,10 @@ import { BiSort } from "react-icons/bi"
 import { BsFilter } from "react-icons/bs"
 import { useNavigate } from "react-router-dom"
 import { SearchInput } from "../../../common/search-input/search-input"
+import { SortDropDown } from "./sort-drop-down/sort-drop-down"
 
 
 export const ListOptions = ({ setIsLoading }: propsType) => {
-    const [isSortModalOpen, setIsSortModalOpen] = useState(false)
-    const toggleIsSortModalOpen = () => setIsSortModalOpen(!isSortModalOpen)
-
-
-    const EL_SORT_CONTAINER = useRef<HTMLDivElement>(null)
-    useOnClickOutside(EL_SORT_CONTAINER, () => setIsSortModalOpen(false))
-
-
-    const navigate = useNavigate()
-    const setSort = ({ key, order }: sortType) => {
-        const url = new URL(window.location.href)
-        const params = new URLSearchParams(url.search)
-        params.set('sKey', key)
-        params.set('sOrder', order)
-        navigate({ search: params.toString() })
-        setIsLoading(true)
-    }
-
-
     return (
         <div className="entities-common-cmp--list-options__container">
             <SearchInput title="חפש לפי כותרת" placeholder="חפש כותרת" />
@@ -38,23 +20,7 @@ export const ListOptions = ({ setIsLoading }: propsType) => {
                 <BsFilter />
             </div>
 
-            <div
-                className={"sort" + (isSortModalOpen ? ' colorize' : '')}
-                title="מיין תצוגה"
-                onClick={toggleIsSortModalOpen}
-                ref={EL_SORT_CONTAINER}>
-                <span className="search-icon"><BiSort /></span>
-
-                {isSortModalOpen && <div className="sort-drop-down-container">
-                    <div className="title">מיין תצוגה</div>
-                    <div className="options-list">
-                        <div className="option" onClick={() => setSort({ key: 'name', order: 'asc' })}> כותרת (א-ת)</div>
-                        <div className="option" onClick={() => setSort({ key: 'name', order: 'desc' })}> כותרת (ת-א)</div>
-                        <div className="option" onClick={() => setSort({ key: 'createdAt', order: 'asc' })}> תאריך הוספה (מהחדש לישן)</div>
-                        <div className="option" onClick={() => setSort({ key: 'createdAt', order: 'desc' })}> תאריך הוספה (מהישן לחדש)</div>
-                    </div>
-                </div>}
-            </div>
+            <SortDropDown setIsLoading={setIsLoading} />
         </div>
     )
 }
@@ -62,10 +28,4 @@ export const ListOptions = ({ setIsLoading }: propsType) => {
 
 type propsType = {
     setIsLoading: React.Dispatch<React.SetStateAction<boolean>>
-}
-
-
-type sortType = {
-    key: string,
-    order: string
 }
---
+++ src/components/entities/common/list-options/sort-drop-down/sort-drop-down.tsx
@@ -0,0 +1,76 @@
+import { useRef, useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { useOnClickOutside } from "../../../../../hooks/use-on-click-outside"
+import { BiSort } from "react-icons/bi"
+
+
+export const SortDropDown = ({ setIsLoading }: propsType) => {
+    const [isSortModalOpen, setIsSortModalOpen] = useState(false)
+    const toggleIsSortModalOpen = () => setIsSortModalOpen(!isSortModalOpen)
+
+
+    const EL_SORT_CONTAINER = useRef<HTMLDivElement>(null)
+    useOnClickOutside(EL_SORT_CONTAINER, () => setIsSortModalOpen(false))
+
+
+    const navigate = useNavigate()
+    const url = new URL(window.location.href)
+    const params = new URLSearchParams(url.search)
+    const setSort = ({ key, order }: sortType) => {
+        params.set('sKey', key)
+        params.set('sOrder', order)
+        navigate({ search: params.toString() })
+        setIsLoading(true)
+    }
+
+
+    const getIsOptionActive = (optionData: { key: string, order: string }) => {
+        return ((optionData.key === params.get('sKey')) && (optionData.order === params.get('sOrder'))) ? true : false
+    }
+
+
+    return (
+        <div
+            className={"entities-common-cmp--sort-drop-down__container " + (isSortModalOpen ? ' colorize' : '')}
+            title="מיין תצוגה"
+            onClick={toggleIsSortModalOpen}
+            ref={EL_SORT_CONTAINER}>
+            <span className="search-icon"><BiSort /></span>
+
+            {isSortModalOpen && <div className="sort-drop-down-container">
+                <div className="title">מיין תצוגה</div>
+                <div className="options-list">
+                    {DATA.map(option => {
+                        const optionData = { key: option.key, order: option.order }
+                        return (
+                            <div
+                                key={option.text}
+                                className={"option" + (getIsOptionActive(optionData) ? ' active' : '')}
+                                onClick={() => setSort(optionData)}
+                            >{option.text}</div>
+                        )
+                    })}
+                </div>
+            </div>}
+        </div>
+    )
+}
+
+
+const DATA = [
+    { text: 'כותרת (א-ת)', key: 'name', order: 'asc' },
+    { text: 'כותרת (ת-א)', key: 'name', order: 'desc' },
+    { text: 'תאריך הוספה (מהחדש לישן)', key: 'createdAt', order: 'asc' },
+    { text: 'תאריך הוספה (מהישן לחדש)', key: 'createdAt', order: 'desc' },
+]
+
+
+type propsType = {
+    setIsLoading: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+
+type sortType = {
+    key: string,
+    order: string
+}
