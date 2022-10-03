461aa5b473e72f603b8906e0046f5b63e2282dd5
O Y <orenyaniv90@gmail.com>
2022-10-03 10:29:37 +0300

    Init game poster preview dropdown

--- src/components/entities/game-poster/game-poster-preview.tsx
+++ src/components/entities/game-poster/game-poster-preview.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { BsThreeDotsVertical } from "react-icons/bs"
 
 import { convertTimestampToDisplayText } from "../../../services/util/convert-timestamp-to-display-text"
@@ -5,20 +6,27 @@ import { convertTimestampToDisplayText } from "../../../services/util/convert-ti
 import { gamePosterType } from "../../../types/game-poster"
 
 import { DisplayBranchIconById } from "../../common/branch-icon/display-branch-icon-by-id"
+import { GamePosterPreviewDropdown } from "./game-poster-preview-drop-down"
 
 
 export const GamePosterPreview = ({ poster }: propsType) => {
     const { entityInfo: { time: { timestamp } } } = poster
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false)
+
+
+    const toggleIsDropdownOpen = () => setIsDropdownOpen(!isDropdownOpen)
+
 
     return (
-        <div className="entities-cmp-game-poster-preview__container">
+        <div className="entities-cmp--game-poster-preview__container">
             <div className="preview-title">
                 <span className="branch-icon-container">
                     <DisplayBranchIconById id={poster.relatedInfo.branchId} className="branch-icon" />
                 </span>
                 <span className="title">{timestamp && convertTimestampToDisplayText(timestamp)}</span>
-                <div className="options-drop-down-container">
+                <div className="options-drop-down-container" onClick={toggleIsDropdownOpen}>
                     <BsThreeDotsVertical />
+                    {isDropdownOpen && <GamePosterPreviewDropdown />}
                 </div>
             </div>
 
