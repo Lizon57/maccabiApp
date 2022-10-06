2672645cd90dde0037dc4d652172bb5a4ddda378
O Y <orenyaniv90@gmail.com>
2022-10-06 20:44:34 +0300

    Increase demo data of signature tntity

--- src/data/entities/signature/signature-db.ts
+++ src/data/entities/signature/signature-db.ts
@@ -78,4 +78,194 @@ export const SIGNATURE_DB: SignatureEntityItem[] = [
 
         images: ['signature_4.png']
     },
+
+    {
+        id: makeId(),
+        relatedInfo: {
+            miniProfile: {
+                displayName: 'קייל אנסינג'
+            },
+            branchId: 'brandId00003',
+        },
+
+        entityInfo: {
+            name: {
+                display: 'קייל אנסינג'
+            },
+            view: 40
+        },
+
+        images: ['signature_2.png', 'signature_4.png']
+    },
+
+    {
+        id: makeId(),
+        relatedInfo: {
+            miniProfile: {
+                displayName: 'יותם בריגר'
+            },
+            branchId: 'brandId00003',
+        },
+
+        entityInfo: {
+            name: {
+                display: 'יותם בריגר'
+            },
+            view: 40
+        },
+
+        images: ['signature_1.png', 'signature_4.png']
+    },
+
+    {
+        id: makeId(),
+        relatedInfo: {
+            miniProfile: {
+                displayName: 'נח וולף'
+            },
+            branchId: 'brandId00003',
+        },
+
+        entityInfo: {
+            name: {
+                display: 'נח וולף'
+            },
+            view: 40
+        },
+
+        images: ['signature_3.png', 'signature_2.png']
+    },
+
+    {
+        id: makeId(),
+        relatedInfo: {
+            miniProfile: {
+                displayName: 'אייל פרולינגר'
+            },
+            branchId: 'brandId00004',
+        },
+
+        entityInfo: {
+            name: {
+                display: 'אייל פרולינגר'
+            },
+            view: 40
+        },
+
+        images: ['signature_1.png', 'signature_2.png']
+    },
+
+    {
+        id: makeId(),
+        relatedInfo: {
+            miniProfile: {
+                displayName: 'פטר קוממניץ\''
+            },
+            branchId: 'brandId00004',
+        },
+
+        entityInfo: {
+            name: {
+                display: 'פטר קוממניץ\''
+            },
+            view: 450
+        },
+
+        images: ['signature_2.png', 'signature_5.png']
+    },
+
+    {
+        id: makeId(),
+        relatedInfo: {
+            miniProfile: {
+                displayName: 'טל בורנשטיין'
+            },
+            branchId: 'brandId00002',
+        },
+
+        entityInfo: {
+            name: {
+                display: 'טל בורנשטיין'
+            },
+            view: 10
+        },
+
+        images: ['signature_3.png', 'signature_5.png']
+    },
+
+    {
+        id: makeId(),
+        relatedInfo: {
+            miniProfile: {
+                displayName: 'יותם הלפרין'
+            },
+            branchId: 'brandId00002',
+        },
+
+        entityInfo: {
+            name: {
+                display: 'יותם הלפרין'
+            },
+            view: 5
+        },
+
+        images: ['signature_5.png', 'signature_1.png']
+    },
+
+    {
+        id: makeId(),
+        relatedInfo: {
+            miniProfile: {
+                displayName: 'ערן זהבי'
+            },
+            branchId: 'brandId00001',
+        },
+
+        entityInfo: {
+            name: {
+                display: 'ערן זהבי'
+            },
+            view: 50
+        },
+
+        images: ['signature_5.png', 'signature_1.png']
+    },
+
+    {
+        id: makeId(),
+        relatedInfo: {
+            miniProfile: {
+                displayName: 'דור פרץ'
+            },
+            branchId: 'brandId00001',
+        },
+
+        entityInfo: {
+            name: {
+                display: 'דור פרץ'
+            },
+            view: 60
+        },
+
+        images: ['signature_3.png', 'signature_2.png']
+    },
+
+    {
+        id: makeId(),
+        relatedInfo: {
+            miniProfile: {
+                displayName: 'דניאל פרץ'
+            },
+            branchId: 'brandId00001',
+        },
+
+        entityInfo: {
+            name: {
+                display: 'דניאל פרץ'
+            },
+            view: 70
+        },
+
+        images: ['signature_4.png', 'signature_2.png']
+    },
 ]
