e6bc2ec3eeb2ed347b117793f57992c1fa2335bc
O Y <orenyaniv90@gmail.com>
2022-10-07 11:43:20 +0300

    Link entity to details page

--- src/components/entities/portal/dynamic-entity-preview/square-entity-preview.tsx
+++ src/components/entities/portal/dynamic-entity-preview/square-entity-preview.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { AiFillEye, AiFillHeart, AiFillStar, AiOutlineHeart } from "react-icons/ai"
 import { FiEdit2 } from "react-icons/fi"
+import { Link } from "react-router-dom"
 
 import { EntityItem } from "../../../../types/entity-item"
 
@@ -20,46 +21,48 @@ export const SquareEntityPreview = ({ item, imagePath }: Props) => {
 
     return (
         <div className="dynamic-entity-preview--square-entity-preview__container">
-            <div className="title">
-                <span className="branch-icon"><DisplayBranchIconById id={relatedInfo.branchId} /></span>
-                <span className="title">{entityInfo.name.display}</span>
-            </div>
+            <Link to={entityInfo.name.display}>
+                <div className="title">
+                    <span className="branch-icon"><DisplayBranchIconById id={relatedInfo.branchId} /></span>
+                    <span className="title">{entityInfo.name.display}</span>
+                </div>
 
-            <div className="image-container">
-                <span
-                    className={"like-icon" + (isLike ? ' active' : '')}
-                    title={isLike ? 'הסר מרשימת המועדפים' : 'הוסף לרשימת המועדפים'}
-                    onClick={toggleIsLike}>
-                    {isLike ? <AiFillHeart /> : <AiOutlineHeart />}
-                </span>
+                <div className="image-container">
+                    <span
+                        className={"like-icon" + (isLike ? ' active' : '')}
+                        title={isLike ? 'הסר מרשימת המועדפים' : 'הוסף לרשימת המועדפים'}
+                        onClick={toggleIsLike}>
+                        {isLike ? <AiFillHeart /> : <AiOutlineHeart />}
+                    </span>
 
-                {TOTAL_IMAGE > 1 && <span className="image-counter" title={`${TOTAL_IMAGE} תמונות תצוגה`}>{TOTAL_IMAGE}</span>}
+                    {TOTAL_IMAGE > 1 && <span className="image-counter" title={`${TOTAL_IMAGE} תמונות תצוגה`}>{TOTAL_IMAGE}</span>}
 
-                <ImageSlider imagePath={imagePath} images={item.images} />
+                    <ImageSlider imagePath={imagePath} images={item.images} />
 
-                <div className="additional-info">
-                    <div className="views-and-rate">
-                        <div className="views" title={`${itemInfo.view} צפיות`}>
-                            <span className="icon"><AiFillEye size={11} /></span>
-                            <span className="text">{itemInfo.view ? itemInfo.view : 'לא נצפה'}</span>
+                    <div className="additional-info">
+                        <div className="views-and-rate">
+                            <div className="views" title={`${itemInfo.view} צפיות`}>
+                                <span className="icon"><AiFillEye size={11} /></span>
+                                <span className="text">{itemInfo.view ? itemInfo.view : 'לא נצפה'}</span>
+                            </div>
+
+                            <div
+                                className="rate"
+                                title={itemInfo.rate.avg
+                                    ? `${itemInfo.rate.avg} כוכבי דירוג (בידי ${itemInfo.rate.raterCount} מדרגים)`
+                                    : 'טרם דורג'}>
+                                <span className="icon"><AiFillStar size={11} /></span>
+                                <span className="text">{itemInfo.rate.avg ? itemInfo.rate.avg : 'ללא דירוג'}</span>
+                            </div>
                         </div>
 
-                        <div
-                            className="rate"
-                            title={itemInfo.rate.avg
-                                ? `${itemInfo.rate.avg} כוכבי דירוג (בידי ${itemInfo.rate.raterCount} מדרגים)`
-                                : 'טרם דורג'}>
-                            <span className="icon"><AiFillStar size={11} /></span>
-                            <span className="text">{itemInfo.rate.avg ? itemInfo.rate.avg : 'ללא דירוג'}</span>
+                        <div className="last-edit-date" title={`נערך לאחרונה לפני שנתיים. סה"כ נערך ${itemInfo.editHistory.total} פעמים`}>
+                            <span className="icon"><FiEdit2 size={11} /></span>
+                            <span className="text">לפני שנתיים</span>
                         </div>
-                    </div>
-
-                    <div className="last-edit-date" title={`נערך לאחרונה לפני שנתיים. סה"כ נערך ${itemInfo.editHistory.total} פעמים`}>
-                        <span className="icon"><FiEdit2 size={11} /></span>
-                        <span className="text">לפני שנתיים</span>
                     </div>
                 </div>
-            </div>
+            </Link>
         </div>
     )
 }
