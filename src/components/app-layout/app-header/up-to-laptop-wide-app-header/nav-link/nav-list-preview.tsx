ea1dc43c9b7c8231a0ef8238503149bf796137b0
O Y <orenyaniv90@gmail.com>
2022-12-12 15:21:29 +0200

    Up-to-laptop nav bar adding title to links

--- src/components/app-layout/app-header/up-to-laptop-wide-app-header/nav-link/nav-list-preview.tsx
+++ src/components/app-layout/app-header/up-to-laptop-wide-app-header/nav-link/nav-list-preview.tsx
@@ -4,14 +4,14 @@ import { FiTriangle } from "react-icons/fi"
 
 
 export const NavListPreview = ({ category, currentCategoryOpen, onCloseMenu, setCurrentCategoryOpen }: Props) => {
-    const resetMenu = () =>{
+    const resetMenu = () => {
         setCurrentCategoryOpen('')
         onCloseMenu()
     }
 
 
     return (
-        <li className="app-header--side-menu__drop-down">
+        <li className="app-header--side-menu__drop-down" title={category.text}>
             <div
                 className={'title' + (currentCategoryOpen === category.id ? ' open' : '')}
                 onClick={() => currentCategoryOpen === category.id
@@ -23,7 +23,7 @@ export const NavListPreview = ({ category, currentCategoryOpen, onCloseMenu, set
 
             <ul className={'drop-down-container' + (currentCategoryOpen === category.id ? ' open' : '')}>
                 {category.childrens.map(link => <li key={link.id} onClick={resetMenu}>
-                    <Link to={link.path}>{link.title}</Link>
+                    <Link to={link.path} title={link.title}>{link.title}</Link>
                 </li>)}
             </ul>
         </li>
