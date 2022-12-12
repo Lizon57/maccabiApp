452fcc1e1ace7413267e5d661b342da167e511ff
O Y <orenyaniv90@gmail.com>
2022-12-12 09:37:23 +0200

    [fix] app-screen fades in and out & functionallity change - show only when has zScreenIndex truthy at store

--- src/components/app-layout/app-screen/app-screen.tsx
+++ src/components/app-layout/app-screen/app-screen.tsx
@@ -2,13 +2,12 @@ import { useStoreSelector } from "../../../hooks/store/use-store-selector"
 
 export const AppScreen = () => {
     const { appScreenZIndex } = useStoreSelector(state => state.appLayout)
-    const STYLE = { zIndex: appScreenZIndex }
+    const style = { zIndex: appScreenZIndex }
 
     return (
-        appScreenZIndex
-            ? <div className="app-layout--app-screen__container" style={STYLE}>
-            </div>
-
-            : null
+        <div
+            className={"app-layout--app-screen__container" + (appScreenZIndex ? ' active' : '')}
+            style={style}>
+        </div>
     )
 }
