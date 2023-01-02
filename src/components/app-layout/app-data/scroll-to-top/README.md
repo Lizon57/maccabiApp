7a3a0d2d851cb73cfd59f66d73131ddb8c369373
O Y <orenyaniv90@gmail.com>
2023-01-02 17:20:34 +0200

    [code-refactor & style] Scroll to top cmp

--- src/components/app-layout/app-data/scroll-to-top/README.md
+++ src/components/app-layout/app-data/scroll-to-top/README.md
@@ -1,9 +1,10 @@
 # ScrollToTop cmp
+![Scroll to top button example](https://i.ibb.co/C8CVZrG/Screenshot-2023-01-02-at-17-19-28.png)
 
 ## Motivation & Context
 
-ScrollToTop cmp will the client a button that help him scroll to top.
+ScrollToTop cmp will provide the client a button that help him scroll back to top.
 
 ## In app use:
 
-Currently, ScrollToTop renders on AppData section, only when cluent scrolled over 30 pixles down.
+Currently, ScrollToTop renders on AppData section, only when client scrolled over 30 pixles down.
--- src/components/app-layout/app-data/scroll-to-top/scroll-to-top.tsx
+++ src/components/app-layout/app-data/scroll-to-top/scroll-to-top.tsx
@@ -2,13 +2,17 @@ import { AiOutlineArrowUp } from "react-icons/ai"
 import { useWindowScrollY } from "../../../../hooks/use-window-scroll-y"
 
 
-const scroll = () => window.scrollTo({ top: 0, behavior: 'smooth' })
+const ON_SCROLL_TO_TOP = () => window.scrollTo({ top: 0, behavior: 'smooth' })
 
 export const ScrollToTop = () => {
-    const shouldRender = (useWindowScrollY() > 30)
+    const shouldRenderButton = (useWindowScrollY() > 30)
 
     return (
-        <div className={"app-data--scrol-to-top__container" + (!shouldRender ? ' deactive' : '')} onClick={scroll} title="חזור לראש הדף">
+        <div
+            className={'app-data--scrol-to-top__container' + (!shouldRenderButton ? ' hide' : '')}
+            onClick={ON_SCROLL_TO_TOP}
+            title="חזור לראש הדף"
+        >
             <AiOutlineArrowUp />
         </div>
     )
