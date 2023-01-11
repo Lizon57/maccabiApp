46c8d31c6bc9f68626757774b1563d9edcd9e787
O Y <orenyaniv90@gmail.com>
2023-01-11 02:12:02 +0200

    [func] Prevent showing active filter chip of branches filter (entity portal) when all branches are visible

--- src/components/entities/portal/active-filter/active-filter-list.tsx
+++ src/components/entities/portal/active-filter/active-filter-list.tsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react"
 
+import { BRANCHES } from "../../../../data/app/supports-branches"
+
 import { filterEntityService } from "../../../../services/entities/filter-entity-service"
 
 import { EntityFilterOption } from "../../../../types/entity/filter/entity-filter-option"
@@ -9,6 +11,8 @@ import { ActiveFilterPreview } from "./active-filter-preview"
 
 export const ActiveFilterList = ({ possibleFiilters, setIsLoading }: Props) => {
     const [activeFilters, setActiveFilters] = useState<EntityFilterOption[]>()
+    const { searchParams: params } = new URL(window.location.href)
+
 
     useEffect(() => {
         const activeFilters = filterEntityService.getActiveFilters(possibleFiilters) as EntityFilterOption[]
@@ -16,7 +20,17 @@ export const ActiveFilterList = ({ possibleFiilters, setIsLoading }: Props) => {
     }, [possibleFiilters])
 
 
-    if (!activeFilters?.length) return null
+    const shouldRender = (() => {
+        if (!((activeFilters?.length === 1) && (activeFilters[0].key === 'relatedInfo.branchIds'))) return true
+        
+        const branchesFromParam = (params.get(activeFilters[0].param) || '').split(',')
+        if (branchesFromParam.length === BRANCHES.length) return false
+        return true
+    })()
+
+    
+    if (!(activeFilters?.length)) return null
+    if (!shouldRender) return null
 
     return (
         <div className="entities-portal--active-filter-list__container">
